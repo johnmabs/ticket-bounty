f9116312fdd80ac65c783862865eaad380c9bef3
John Mabiala <johnmabiala@hotmail.com>
2026-01-12 15:51:47 +0100

    Make some fix

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -4,7 +4,7 @@ import { Ticket } from "@/features/ticket/types";
 export async function getTickets(): Promise<Ticket[]> {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  throw new Error("Failed to fetch tickets");
+  //throw new Error("Failed to fetch tickets");
 
   return new Promise((resolve) => {
     resolve(initialTickets);
